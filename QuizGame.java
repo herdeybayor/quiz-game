8b0ef9f75cf73ce8dafb4afdb389eb4078e4ff75
herdeybayor <shereefdeen4real@gmail.com>
2023-11-24 01:15:50 +0100

    docs: add comments

--- QuizGame.java
+++ QuizGame.java
@@ -1,7 +1,7 @@
-// QuizGame.java
 import java.util.Scanner;
 
 public class QuizGame {
+	// declare variable to store the questions
     private String[] questions = {
             "What is the capital of France?",
             "Which planet is known as the Red Planet?",
@@ -10,6 +10,7 @@ public class QuizGame {
             "What is the powerhouse of the cell?"
     };
 
+	// declare variable to store the options
     private String[][] options = {
             {"A. Paris", "B. Berlin", "C. Madrid", "D. Rome"},
             {"A. Venus", "B. Mars", "C. Jupiter", "D. Saturn"},
@@ -18,34 +19,41 @@ public class QuizGame {
             {"A. Nucleus", "B. Mitochondria", "C. Ribosome", "D. Endoplasmic Reticulum"}
     };
 
+	// declare variable to store the correct answers
     private char[] correctAnswers = {'A', 'B', 'A', 'A', 'B'};
 
-    private int totalQuestions = questions.length;
-    private int correctResponses = 0;
+    private int totalQuestions = questions.length; // declare variable to store the total number of questions
+    private int correctResponses = 0; // declare variable to store the number of correct responses
 
     public void startQuiz() {
+		// Initialize scanner to read user input
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to the Quiz Game! Please answer the following questions:");
 
+		// Loop through the questions
         for (int i = 0; i < totalQuestions; i++) {
             System.out.println("\n" + questions[i]);
+
+			// Loop through the options
             for (String option : options[i]) {
                 System.out.println(option);
             }
 
             System.out.print("Your answer (A, B, C, or D): ");
-            char userAnswer = scanner.next().toUpperCase().charAt(0);
+            char userAnswer = scanner.next().toUpperCase().charAt(0); // Read user input and convert to uppercase
 
-            if (userAnswer == correctAnswers[i]) {
+            if (userAnswer == correctAnswers[i]) { // Check if the user's answer is correct
                 System.out.println("Correct!");
-                correctResponses++;
+                correctResponses++; // Increment the number of correct responses
             } else {
-                System.out.println("Incorrect. The correct answer is " + correctAnswers[i] + ".");
+                System.out.println("Incorrect. The correct answer is " + correctAnswers[i] + "."); // Display the correct answer
             }
         }
 
+		// Calculate the score percentage
         double scorePercentage = (double) correctResponses / totalQuestions * 100;
+		// Display the final score
         System.out.println("\nYour final score: " + correctResponses + "/" + totalQuestions + " (" + scorePercentage + "%)");
 
         scanner.close();
